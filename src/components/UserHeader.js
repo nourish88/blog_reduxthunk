dabe6c927f5567312753ed9dabc7650bfd889709
jugadev <jugadev@outlook.com>
2021-09-01 10:43:36 +0300

    aa

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -6,11 +6,14 @@ class UserHeader extends React.Component {
   componentDidMount() {
     this.props.fetchUser(this.props.userId);
   }
+
   render() {
-    const user = this.props.users.find(user => user.id === this.props.userId);
+    const { user } = this.props;
+
     if (!user) {
       return null;
     }
+
     return (
       <div className="header">
         {user.name}
@@ -19,8 +22,8 @@ class UserHeader extends React.Component {
   }
 }
 
-const mapStateToProps = state => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,11 +1,6 @@
 export default (state = [], action) => {
-  //   if (action.type === "FETCH_POSTS") {
-  //     return action.payload;
-  //   }
-  //   return state;
-
   switch (action.type) {
-    case "FETCH_POSTS":
+    case 'FETCH_POSTS':
       return action.payload;
     default:
       return state;
